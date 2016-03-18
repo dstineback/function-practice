52699887bd236568347888f17eb1b8e56988510e
dstineback <dstineback@gmail.com>
2016-03-18 09:13:26 -0700

    update after Q1

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('javascript is working');
+
+//function #1//
+function sum(numOne, numTwo) {
+  var total = (numOne + numTwo);
+  console.log('The sum of ' + numOne + ' and ' + numTwo + ' is ' + total + '.');
+}
